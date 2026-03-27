6b254f5f462996128fb5ccc595c59d9fcafaa619
Drosalesa <david.rosales1698@gmail.com>
2026-03-27 12:19:24 -0600

    Add functions to  edit profile modal

--- scripts/index.js
+++ scripts/index.js
@@ -28,3 +28,48 @@ const initialCards = [
 initialCards.forEach(function (card) {
   console.log(card);
 });
+
+const profileEditBtn = document.querySelector(".profile__edit-button");
+const profileEditModal = document.querySelector("#edit-popup");
+const closeEditModalBtn = profileEditModal.querySelector(".popup__close");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const profileNameInput = profileEditModal.querySelector(
+  ".popup__input_type_name",
+);
+const profileDescInput = profileEditModal.querySelector(
+  ".popup__input_type_description",
+);
+
+function openModal(modal) {
+  modal.classList.add("popup_is-opened");
+}
+
+function closeModal(modal) {
+  modal.classList.remove("popup_is-opened");
+}
+
+function fillProfileForm() {
+  profileNameInput.value = profileTitle.textContent;
+  profileDescInput.value = profileDescription.textContent;
+}
+
+function handleOpenEditModal(modal) {
+  openModal(modal);
+  fillProfileForm();
+}
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileTitle.textContent = profileNameInput.value;
+  profileDescription.textContent = profileDescInput.value;
+  closeModal(profileEditModal);
+}
+
+profileEditBtn.addEventListener("click", () =>
+  handleOpenEditModal(profileEditModal),
+);
+
+profileEditModal.addEventListener("submit", handleProfileFormSubmit);
+
+closeEditModalBtn.addEventListener("click", () => closeModal(profileEditModal));
