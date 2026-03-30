5c618543a7cb8b5456d886cbcc92e93c711bd023
Drosalesa <david.rosales1698@gmail.com>
2026-03-30 17:29:07 -0600

    Add fuctions to buttos, add modals

--- README.md
+++ README.md
@@ -2,8 +2,10 @@
 
 # Página para agregar, eliminar y dar me gusta a fotos de lugares visitados "Around The US".
 
-# Etapa 2: Perfil modal
+# Etapa final
 
-# Se agregó función de abrir y cerrar menú de edición de perfil y guardado de cambios
+# El sitio ahora utiliza un template para crear nuevas tarjetas.
+
+# El sitio permite también eliminar tarjetas, dar me gusta y abre un pop-up de la imagen en la tarjeta al hacer click.
 
 # https://drosalesa.github.io/web_project_around_es/
--- scripts/index.js
+++ scripts/index.js
@@ -25,10 +25,6 @@ const initialCards = [
   },
 ];
 
-initialCards.forEach(function (card) {
-  console.log(card.name);
-});
-
 const profileEditBtn = document.querySelector(".profile__edit-button");
 const profileEditModal = document.querySelector("#edit-popup");
 const closeEditModalBtn = profileEditModal.querySelector(".popup__close");
@@ -40,6 +36,24 @@ const profileNameInput = profileEditModal.querySelector(
 const profileDescInput = profileEditModal.querySelector(
   ".popup__input_type_description",
 );
+const cardTemplate = document
+  .querySelector("#card__template")
+  .content.querySelector(".card");
+const cardContainer = document.querySelector(".cards__list");
+
+const profileAddBtn = document.querySelector(".profile__add-button");
+const profileAddModal = document.querySelector("#new-card-popup");
+const closeAddModalBtn = profileAddModal.querySelector(".popup__close");
+const newCardTitleInput = profileAddModal.querySelector(
+  ".popup__input_type_card-name",
+);
+const newCardImageInput = profileAddModal.querySelector(
+  ".popup__input_type_url",
+);
+const imageModal = document.querySelector("#image-popup");
+const closeImageModal = imageModal.querySelector(".popup__close");
+const popUpImage = imageModal.querySelector(".popup__image");
+const popUpCaption = imageModal.querySelector(".popup__caption");
 
 function openModal(modal) {
   modal.classList.add("popup_is-opened");
@@ -71,5 +85,54 @@ profileEditBtn.addEventListener("click", () =>
 );
 
 profileEditModal.addEventListener("submit", handleProfileFormSubmit);
-
 closeEditModalBtn.addEventListener("click", () => closeModal(profileEditModal));
+
+function getCardElement(
+  name = "Sin título",
+  link = "../images/placeholder.jpg",
+) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+  const cardLikeBtn = cardElement.querySelector(".card__like-button");
+  const cardDeleteBtn = cardElement.querySelector(".card__delete-button");
+
+  cardTitle.textContent = name;
+  cardImage.setAttribute("src", link);
+  cardImage.setAttribute("alt", name);
+  cardLikeBtn.addEventListener("click", (evt) => {
+    evt.target.classList.toggle("card__like-button_is-active");
+  });
+  cardDeleteBtn.addEventListener("click", (evt) => {
+    evt.target.parentElement.remove();
+  });
+  cardImage.addEventListener("click", () => {
+    popUpImage.setAttribute("src", link);
+    popUpImage.setAttribute("alt", name);
+    popUpCaption.textContent = name;
+    openModal(imageModal);
+  });
+  return cardElement;
+}
+
+function renderCardElement(name, link) {
+  const cardEl = getCardElement(name, link);
+  cardContainer.prepend(cardEl);
+}
+
+initialCards.forEach((card) => {
+  renderCardElement(card.name, card.link);
+});
+
+profileAddBtn.addEventListener("click", () => openModal(profileAddModal));
+closeAddModalBtn.addEventListener("click", () => closeModal(profileAddModal));
+closeImageModal.addEventListener("click", () => closeModal(imageModal));
+
+function handleCardFormSubmit(evt) {
+  evt.preventDefault();
+  renderCardElement(newCardTitleInput.value, newCardImageInput.value);
+  closeModal(profileAddModal);
+  newCardTitleInput.value = "";
+  newCardImageInput.value = "";
+}
+profileAddModal.addEventListener("submit", handleCardFormSubmit);
